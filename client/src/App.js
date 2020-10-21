da862a52f96c8c984ff75a1ce2cc02cbf39a6f25
layc41 <64671431+layc41@users.noreply.github.com>
2020-10-21 18:51:19 -0500

    added code to server auth, client auth, and server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
 
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
 
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
