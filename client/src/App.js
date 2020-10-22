918fed65f8294eac282d2f27aa328ed57905d128
asaldana1108 <asaldana1108@gmail.com>
2020-10-21 19:03:03 -0500

    LoginForm, Navbar, and SignupForm components added and tested in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
+import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+import NoMatch from './pages/NoMatch';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Navbar} />
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/signup" component={SignupForm} />
+
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+        
+      </div>
+    </Router>
+  </ApolloProvider>
   );
 }
 
