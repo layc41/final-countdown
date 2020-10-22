fb2ade0ff87d50ae0bbe30de47632127f82b08d2
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-10-22 11:00:57 -0500

    api functioning with posters

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -54,6 +54,6 @@ export const getMe = (token) => {
   
   //https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
   export const searchMovieDb = (query) => {
-    // const API_KEY = process.env.REACT_APP_API_KEY;
-    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`);
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`);
   };
