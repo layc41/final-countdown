a995d2709d150997775474899d94d76e65ac2157
darshnablah <dwright758@gmail.com>
2020-10-25 18:50:46 -0500

    changed footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,27 +2,13 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer class='bg-secondary'>
-      <ul class="flex-row justify-space-around ">
-        <li>
-          <a href='https://github.com/tbreazier'>Tom</a>
-        </li>
-        <li>
-          <a href='https://github.com/joshuaweisbrodtorres'>Joshua</a>
-        </li>
-        <li>
-          <a href='https://github.com/DariusJWright'>Darius</a>
-        </li>
-        <li>
-          <a href='https://github.com/layc41'>Claudia</a>
-        </li>
-        <li>
-          <a href='https://github.com/asaldana1108'>Alvaro</a>
-        </li>
-        <li>
-          <a href='#'>Joey</a>
-        </li>
-      </ul>
+    <footer class='bg-secondary flex-row justify-space-around'>
+      <a href='https://github.com/tbreazier'>Tom</a>
+      <a href='https://github.com/joshuaweisbrodtorres'>Joshua</a>
+      <a href='https://github.com/DariusJWright'>Darius</a>
+      <a href='https://github.com/layc41'>Claudia</a>
+      <a href='https://github.com/asaldana1108'>Alvaro</a>
+      <a href='#'>Joey</a>
     </footer>
   );
 }
