d55a5f8f8a3bed73e84995a655af7193996a03d9
darshnablah <dwright758@gmail.com>
2020-10-26 18:02:55 -0500

    commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -43,15 +43,14 @@ const resolvers = {
             
             return { token, user };
         },
-        saveMovie: async (parent, { movieId }, context) => {
-            
+        saveMovie: async (parent, { movie }, context) => {
             if (context.user) {
                 const updatedMovies = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $push: { savedMovies: movieId } },
+                    { $push: { savedMovies: movie } },
                     { new: true }
                 ).populate('savedMovies');
-                return updatedMovies
+                return updatedMovies;
             }
             throw new AuthenticationError('You need to be logged in!');
         },
