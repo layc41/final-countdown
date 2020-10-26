f05204512af4a3eafafdcee2cab8cc455d4c41ee
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-10-25 19:48:33 -0500

    logout functionality added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,19 +11,31 @@ function Navbar() {
     return (
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
             <div className="container flex-row justify-space-between-lg justify-center align-center">
-                <Link to="/">
-                    <h1 className='title'>Movie Lot</h1>
-                </Link>
-
-                <nav className="text-center">
+            <Link to="/">
+                <h1 className='title'>Movie Lot</h1>
+            </Link>
+            
+            <nav className="text-center">
+            {Auth.loggedIn() ? (
+                <>
+                <a href="/" onClick={logout} className='logout-link'>
+                Logout
+                </a>
+                </>
+                ) : (
+                    <>
+                    <div>
                     <Link to="/login" className='login-link'>Login</Link>
-                </nav>
-                <nav className="text-center">
-                <Link to="/signup" className='signup-link'>Signup</Link>
-                </nav>
-            </div>
-        </header>
-    );
-}
+                    </div>
+                    <div>
+                    <Link to="/signup" className='signup-link'>Signup</Link>
+                    </div>
+                    </>
+                    )}
+                    </nav>
+                    </div>
+                    </header>
+                    );
+                }
 
 export default Navbar; 
